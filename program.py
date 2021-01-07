207e08695f099843f6bdabf8c8ac9d9c15f5c533
tobi-byte <brandnertobi1@gmail.com>
2021-01-07 10:59:31 +0100

    Version 3

--- program.py
+++ program.py
@@ -1,18 +1,34 @@
+from flask import Flask 
+from flask import jsonify
 import time
 from datetime import datetime
 import board
 from busio import I2C
 import adafruit_bme680
 
+
 i2c = I2C(board.SCL, board.SDA)
 bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
 
-while True:
-    print("Uhrzeit:", datetime.utcnow().isoformat())
-    print("Temperatur:" ,bme680.temperature,"°C") # Grad Celsius
-    print("Gas:",bme680.gas,"Ohm") # Ohm
-    print("Relative Luftfeuchtigkeit:",bme680.relative_humidity,"%") # %
-    print("Luftdruck:",bme680.pressure,"hPa") # hPa
-    print("Höhe:",bme680.altitude,"m") # m 
-    print ("---")
-    time.sleep(60)
+bme680.sea_level_pressure = 1012.0
+
+def getData():
+    return {
+        'time': datetime.utcnow().isoformat(),
+                'temperature': bme680.temperature,
+                'gas': bme680.gas,
+                'humidity': bme680.relative_humidity,
+                'pressure': bme680.pressure,
+                'altitude': bme680.altitude
+        } 
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index() : 
+    return jsonify(getData())
+    
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0.', port=5000)
