29b3fbf33fe35a3105ba5e431b6d490f24b3cc04
tobi-byte <brandnertobi1@gmail.com>
2021-01-07 10:14:59 +0100

    Version 2

--- program.py
+++ program.py
@@ -5,10 +5,14 @@ from busio import I2C
 import adafruit_bme680
 
 i2c = I2C(board.SCL, board.SDA)
-bme680 = adafruit_bme680.adafruit_BME680_I2C(i2c, debug=False)
+bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
 
-print("Temperatur: " + bme680.temperature + " °C") # Grad Celsius
-print("Gas: " + bme680.gas + "Ohm") # Ohm
-print("Relative Luftfeuchtigkeit: " + relative_humidity + " %") # %
-print("Luftdruck: " + bme680.pressure + " hPa") # hPa
-print("Meter: " + bme680.altitude + " m") # m 
+while True:
+    print("Uhrzeit:", datetime.utcnow().isoformat())
+    print("Temperatur:" ,bme680.temperature,"°C") # Grad Celsius
+    print("Gas:",bme680.gas,"Ohm") # Ohm
+    print("Relative Luftfeuchtigkeit:",bme680.relative_humidity,"%") # %
+    print("Luftdruck:",bme680.pressure,"hPa") # hPa
+    print("Höhe:",bme680.altitude,"m") # m 
+    print ("---")
+    time.sleep(60)
